991ed177046e8b857980f5627c810cc32468b308
Syahrizal As <alisadikinsyahrizal@gmail.com>
2024-01-29 09:38:47 +0700

    first commit

---
+++ app/Models/Category.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'image', 'name', 'description'
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/categories/' . $value),
+        );
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'image', 'barcode', 'title', 'description', 'buy_price', 'sell_price', 'category_id', 'stock'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/products/' . $value),
+        );
+    }
+}
