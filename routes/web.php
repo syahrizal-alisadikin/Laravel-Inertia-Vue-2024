f82cd421d2d860796911c2fdb2b99e738abc3d48
Syahrizal As <alisadikinsyahrizal@gmail.com>
2024-01-29 10:52:26 +0700

    update permission

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,13 @@ Route::prefix('apps')->group(function () {
         //route permissions
         Route::get('/permissions', \App\Http\Controllers\Apps\PermissionController::class)->name('apps.permissions.index')
             ->middleware('permission:permissions.index');
+
+        //route resource roles
+        Route::resource('/roles', \App\Http\Controllers\Apps\RoleController::class, ['as' => 'apps'])
+            ->middleware('permission:roles.index|roles.create|roles.edit|roles.delete');
+
+        //route resource users
+        Route::resource('/users', \App\Http\Controllers\Apps\UserController::class, ['as' => 'apps'])
+            ->middleware('permission:users.index|users.create|users.edit|users.delete');
     });
 });
