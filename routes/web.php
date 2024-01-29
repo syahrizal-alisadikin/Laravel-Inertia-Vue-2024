ae1dceac62aef4bf987b5d0d3e1fb9bec3abef37
Syahrizal As <alisadikinsyahrizal@gmail.com>
2024-01-29 21:02:55 +0700

    baru ke produk

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,17 @@ Route::prefix('apps')->group(function () {
         //route resource users
         Route::resource('/users', \App\Http\Controllers\Apps\UserController::class, ['as' => 'apps'])
             ->middleware('permission:users.index|users.create|users.edit|users.delete');
+
+        //route resource categories
+        Route::resource('/categories', \App\Http\Controllers\Apps\CategoryController::class, ['as' => 'apps'])
+            ->middleware('permission:categories.index|categories.create|categories.edit|categories.delete');
+
+        //route resource products
+        Route::resource('/products', \App\Http\Controllers\Apps\ProductController::class, ['as' => 'apps'])
+            ->middleware('permission:products.index|products.create|products.edit|products.delete');
+
+        //route resource customers
+        Route::resource('/customers', \App\Http\Controllers\Apps\CustomerController::class, ['as' => 'apps'])
+            ->middleware('permission:customers.index|customers.create|customers.edit|customers.delete');
     });
 });
